02707322ede21b6105758abab455ffcd981227e5
wesleycode <kako.araujo.24@gmail.com>
2020-10-05 22:07:05 -0300

    mudanças basicas

--- src/main/java/controller/PesquisaController.java
+++ src/main/java/controller/PesquisaController.java
@@ -15,30 +15,30 @@ import java.util.List;
 @RequestScoped
 public class PesquisaController implements Serializable {
 
-    private List<Produto>todosProdutos;
+    private List<Produto> todosProdutos;
     private List<Produto> produtosAtuais;
     private List<Integer> botoesPesquisa;
 
-    public PesquisaController(){
+    public PesquisaController() {
         try {
-             todosProdutos = new ProdutoBO().listarTodos();
-             /* esse listar varia de acordo com os filtros escolhidos, EX: apenas Notebooks*/
+            todosProdutos = new ProdutoBO().listarTodos();
+            /* esse listar varia de acordo com os filtros escolhidos, EX: apenas Notebooks*/
 
-             for (int x=0;x < 15 ; x++){//Lembrando o 15 é o total de produtos que serão mostrados por pag
-                 produtosAtuais.set(x, todosProdutos.get(x));
-             }//inicio start pag 1
+            for (int x = 0; x < 15; x++) {//Lembrando o 15 é o total de produtos que serão mostrados por pag
+                produtosAtuais.set(x, todosProdutos.get(x));
+            }//inicio start pag 1
 
-             for(int x=0;x < 9; x++){//de acordo com o total de numero de botoes que vai ter na pag
-                 botoesPesquisa.set(x,x);
-             }
+            for (int x = 0; x < 9; x++) {//de acordo com o total de numero de botoes que vai ter na pag
+                botoesPesquisa.set(x, x);
+            }
         } catch (Exception e) {
             System.out.println("Deu ruim chefia :(");
         }
         /*
-        * Ao ir para pagina web de pesquisa ele já efetua o contrutor e já efetua o set dos valores ai
-        *
-        * Cada uma das Div de Produtos no site vai ter seu valores baseados nesses valores de produtoAtuais
-        * */
+         * Ao ir para pagina web de pesquisa ele já efetua o contrutor e já efetua o set dos valores ai
+         *
+         * Cada uma das Div de Produtos no site vai ter seu valores baseados nesses valores de produtoAtuais
+         * */
     }
 
     public List<Produto> getTodosProdutos() {
@@ -65,20 +65,19 @@ public class PesquisaController implements Serializable {
         this.botoesPesquisa = botoesPesquisa;
     }
 
-    public void MudarPag(int Numero_da_pag_Atual){
-        for(int x=0;x<15;x++){
-            produtosAtuais.set(x,todosProdutos.get(x + (15 * (Numero_da_pag_Atual - 1))));
+    public void MudarPag(int numeroDaPagAtual) {
+        for (int x = 0; x < 15; x++) {
+            produtosAtuais.set(x, todosProdutos.get(x + (15 * (numeroDaPagAtual - 1))));
             //atualização dos Produtos por pagina
         }
 
         //Mudar Botões
         int y = -4;
-        for (int x=0;x<9;x++){
-            botoesPesquisa.set(x,Numero_da_pag_Atual-y);
+        for (int x = 0; x < 9; x++) {
+            botoesPesquisa.set(x, numeroDaPagAtual - y);
             y++;
             //atualiza os valores dos botoes da pag
         }
-
 
     }
 
